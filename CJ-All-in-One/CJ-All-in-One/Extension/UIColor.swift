b301d1f86ce05c0393d3f4495b07d560a1ce0a4d
muzee99 <muzee99@pusan.ac.kr>
2022-07-26 19:23:08 +0900

    merge

--- CJ-All-in-One/CJ-All-in-One/Extension/UIColor.swift
+++ CJ-All-in-One/CJ-All-in-One/Extension/UIColor.swift
@@ -41,3 +41,26 @@ extension UIColor {
     class var customLightGray : UIColor { UIColor(hex: 0xCCCCCC) }
 
 }
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+    
+    class var firstRowBackgroundColor: UIColor {return UIColor(rgb: 0xD9D9D9) }
+    class var tableTitleTextColor: UIColor {return UIColor(rgb: 0xAEAEAE) }
+    class var tableContentTextColor: UIColor {return UIColor(rgb: 0x7E7E7E) }
+    class var disableButtonColor: UIColor {return UIColor(rgb: 0xCCCCCC) }
+}
