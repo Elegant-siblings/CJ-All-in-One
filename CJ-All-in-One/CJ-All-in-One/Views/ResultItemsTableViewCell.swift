b7aa9cfb0a2d059df5cfbfc3568eb1eb1b559231
muzee99 <muzee99@pusan.ac.kr>
2022-07-27 01:41:44 +0900

    Result View layout

---
+++ CJ-All-in-One/CJ-All-in-One/Views/ResultItemsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ResultItemsTableViewCell.swift
+//  CJ-All-in-One
+//
+//  Created by 안현주 on 2022/07/27.
+//
+
+import UIKit
+
+class ResultItemsTableViewCell: UITableViewCell {
+    
+    static let identifier = "ResultItemsTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
