46fa78818a0958e6bbad01fecb37e5d1062c92eb
muzee99 <muzee99@pusan.ac.kr>
2022-08-01 15:11:35 +0900

    comlete signin and signup

--- CJ-All-in-One/CJ-All-in-One/Views/SignInUp/SignInViewController.swift
+++ CJ-All-in-One/CJ-All-in-One/Views/SignInUp/SignInViewController.swift
@@ -60,8 +60,16 @@ class SignInViewController: UIViewController {
             buttonSignUp
         ])
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification, object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification, object: nil
+        )
         
         
         headerView.snp.makeConstraints { make in
